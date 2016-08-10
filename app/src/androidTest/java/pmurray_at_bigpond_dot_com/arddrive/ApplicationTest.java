669db39c4eecd8832d9d5468e3fde768d74f1417
Paul Murray <pmurray@bigpond.com>
2016-08-10 21:02:22 +1000

    New Project

---
+++ app/src/androidTest/java/pmurray_at_bigpond_dot_com/arddrive/ApplicationTest.java
@@ -0,0 +1,13 @@
+package pmurray_at_bigpond_dot_com.arddrive;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
