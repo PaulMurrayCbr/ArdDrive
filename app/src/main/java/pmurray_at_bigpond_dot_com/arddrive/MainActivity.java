a011e19746e1cf25905592e360a348abb1c08b90
Paul Murray <pmurray@bigpond.com>
2016-08-10 22:19:38 +1000

    Select device activity.

--- app/src/main/java/pmurray_at_bigpond_dot_com/arddrive/MainActivity.java
+++ app/src/main/java/pmurray_at_bigpond_dot_com/arddrive/MainActivity.java
@@ -1,8 +1,13 @@
 package pmurray_at_bigpond_dot_com.arddrive;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
@@ -10,6 +15,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
+    private static final int PERMISSION_REQUEST_BLUETOOTH_CALLBACK_CODE = 0xBEEF + 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,15 +23,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 
     @Override
@@ -43,10 +40,59 @@ public class MainActivity extends AppCompatActivity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.action_select_device) {
+            chooseBluetooth();
             return true;
         }
 
         return super.onOptionsItemSelected(item);
     }
+
+    protected void chooseBluetooth() {
+        // Check if the Camera permission has been granted
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.BLUETOOTH)
+                == PackageManager.PERMISSION_GRANTED) {
+
+//            startActivity(new Intent(this, Main2Activity.class));
+            startActivity(new Intent(this, SelectDevice.class));
+        } else {
+            requestBluetoothPermission();
+        }
+    }
+
+    protected void requestBluetoothPermission() {
+        if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.BLUETOOTH)) {
+            Snackbar.make(findViewById(android.R.id.content), "show rationale", Snackbar.LENGTH_LONG).show();
+            Snackbar.make(findViewById(android.R.id.content), R.string.bluetoothRationale, Snackbar.LENGTH_INDEFINITE)
+                    .setAction("OK", new View.OnClickListener() {
+                        @Override
+                        public void onClick(View view) {
+                            chooseBluetooth();
+                        }
+                    })
+                    .setAction("Cancel", null)
+                    .show();
+        } else {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.BLUETOOTH},
+                    PERMISSION_REQUEST_BLUETOOTH_CALLBACK_CODE);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String permissions[], @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case PERMISSION_REQUEST_BLUETOOTH_CALLBACK_CODE: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    chooseBluetooth();
+                } else {
+                    Snackbar.make(findViewById(android.R.id.content), R.string.bluetoothRefused, Snackbar.LENGTH_INDEFINITE)
+                            .show();
+                }
+            }
+            break;
+        }
+    }
+
+
 }
